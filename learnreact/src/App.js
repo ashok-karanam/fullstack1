81cdf5d9a6af929b19382c7fc93de2b29d501ed5
Ashok Karanam <ashok.karanam6@gmail.com>
2020-03-09 08:01:32 +0530

    secondcommit

--- learnreact/src/App.js
+++ learnreact/src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
- 
+import FirstComponent from './components/learning-examples/FirstComponent'; 
+import SecondComponent from './components/learning-examples/SecondComponent';
+import ThirdComponent from './components/learning-examples/ThirdComponent';
+
 class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <LearningComponents />
+      </div>
+    );
+  }
+}
+
+class LearningComponents extends Component {
   render() {
     return (
       <div className="App">
         FULL STACK JAVA AND REACT APP
         <FirstComponent />
+        <SecondComponent />
+        <ThirdComponent />
       </div>
     );
   }
 }
-// Class Component
-class FirstComponent extends Component {
-  render() {
-    return (
-      <div className="firstComponent">
-      FIRST COMPONENT
-      </div>
-    );
-  }
-}
+
+
 
 export default App;
---
+++ learnreact/src/components/learning-examples/ThirdComponent.jsx
@@ -0,0 +1,12 @@
+import React, {component} from 'react';
+
+// Functional Component
+function ThirdComponent() {
+    return(
+      <div className="thirdComponent">
+          Third Component
+      </div>
+    )
+}
+
+export default ThirdComponent
