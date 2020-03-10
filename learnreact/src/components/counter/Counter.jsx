d18a881d4c5d84f3df9128112c8b11ae19fe655f
Ashok Karanam <ashok.karanam6@gmail.com>
2020-03-10 08:50:57 +0530

    fourthcommit

--- learnreact/src/components/counter/Counter.jsx
+++ learnreact/src/components/counter/Counter.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './Counter.css';
 
-function Counter() {
-    return(
+class Counter extends Component {
+    // Define initial state in a constructor
+    // state => counter 0
+   constructor() {
+       super();
+       this.state = {
+           counter : 0
+       }
+       this.increment = this.increment.bind(this);
+   }
+
+    render() {
+      return(
         <div className="counter">
           Counter Component
-          <button onClick={increment}>+ 1</button>
-          <span className="count">0</span>
+          <button onClick={this.increment}>+ 1</button>
+          <span className="count">{this.state.counter}</span>
         </div>
-    )
+      )
+    }
+    increment() { // Update state -- counter++
+        this.setState({
+            counter : this.state.counter + 1
+        }); 
+    }
 }
-
-function increment() {
-    console.log('Increment');
-}
+//https://www.freecodecamp.org/news/this-is-why-we-need-to-bind-event-handlers-in-class-components-in-react-f7ea1a6f93eb/
 
 export default Counter
